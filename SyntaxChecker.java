2ff41aabff71458d29c3889898da75266965412f
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-22 20:47:49 -0700

    Update SyntaxChecker.java

--- SyntaxChecker.java
+++ SyntaxChecker.java
@@ -13,20 +13,62 @@ public class SyntaxChecker
 
  public SyntaxChecker()
  {
+  exp = "";
+   symbols = new Stack<Character>();
  }
 
  public SyntaxChecker(String s)
  {
+   exp = s;
+   symbols = new Stack<Character>();
  }
  
  public void setExpression(String s)
  {
+  exp = s;
+   while(!symbols.empty())
+   {
+     symbols.pop();
+   }
  }
+ 
+ public boolean check(Character c)
+  {
+    Character opposite = null;
+    c = c.charValue();
+    switch(c)
+    {
+      case '}': opposite = '{'; break;
+      case ')': opposite = '('; break;
+      case '>': opposite = '<'; break;
+      case ']': opposite = '['; break;
+    }
+    return symbols.peek()==opposite;
+  }
 
  public boolean checkExpression()
  {
-  return false;
+  for(int i = 0; i < exp.length(); i++)
+  {
+    char c = exp.charAt(i);
+    if(c=='{'||c=='}'||c=='<'||c=='>'||c=='('||c==')'||c=='['||c==']')
+    {
+      if(!symbols.empty() && check((Character)exp.charAt(i)))
+        symbols.pop();
+      else
+        symbols.push(exp.charAt(i));
+    }
+  }
+  return symbols.empty();
  }
 
  //write a toString
+ public String toString()
+  {
+    if(checkExpression())
+    {
+      return exp+" is correct.\n";
+    }
+    return exp+" is incorrect.\n";
+  }
 }
